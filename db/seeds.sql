2fff1022689d728ef314f39a82e33caef14b6df2
Bounty556 <jacobmayday@gmail.com>
2020-06-30 17:02:56 -0600

    Seeded db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured) VALUES("Mega Sloppy Joe", false);
+INSERT INTO burgers(burger_name, devoured) VALUES("Triple Bacon Double Stacker", true);
+INSERT INTO burgers(burger_name, devoured) VALUES("Mega Sloppy Joe Burger", false);
